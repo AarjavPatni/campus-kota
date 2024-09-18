7dc2c39a2ccba3044113e5a18cec77b5b6bccb00
Aarjav <68058926+AarjavPatni@users.noreply.github.com>
2024-09-18 15:45:43 -0400

    Add BillGenerator

--- pages/studentList.js
+++ pages/studentList.js
@@ -1,8 +1,10 @@
+import { BillGenerator } from "@/components/BillGenerator";
 import { StudentList } from "@/components/StudentList";
 
 export default function Home() {
   return (
     <div>
+      <BillGenerator />
       <StudentList />
     </div>
   );

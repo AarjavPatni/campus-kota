5cb5435ba6d0a7e44dfbc978a72a3004b965dc98
Aarjav <68058926+AarjavPatni@users.noreply.github.com>
2025-02-11 00:08:55 -0500

    Add security deposit handling in CollectionForm

--- components/CollectionForm.jsx
+++ components/CollectionForm.jsx
@@ -213,6 +213,7 @@ const CollectionForm = ({
         ...formik.values,
         ...restCollection,
         ...restStudent,
+        security_deposit: collectionDetails?.security_deposit ?? studentDetails?.security_deposit ?? 0,
         monthly_charge: collectionDetails?.monthly_charge ?? (studentDetails ? 
           (studentDetails.monthly_rent || 0) + 
           (studentDetails.laundry_charge || 0) + 
@@ -222,6 +223,7 @@ const CollectionForm = ({
       } : {
         ...formik.values,
         ...restStudent,
+        security_deposit: studentDetails?.security_deposit ?? 0,
         monthly_charge: studentDetails ? 
           (studentDetails.monthly_rent || 0) + 
           (studentDetails.laundry_charge || 0) + 
